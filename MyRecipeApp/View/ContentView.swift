ea56e7f220f6e001a327071529c56b8463a1158b
Serezha <evan32@yandex.ru>
2023-03-31 14:05:24 +0600

    Change the headnote a bit

--- MyRecipeApp/View/ContentView.swift
+++ MyRecipeApp/View/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                     }
                 }
             }
-            .navigationTitle("All recipes")
+            .navigationTitle("My recipes")
         }
     }
 }
